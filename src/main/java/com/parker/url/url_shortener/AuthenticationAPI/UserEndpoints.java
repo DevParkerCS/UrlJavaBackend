87b90d2e2b705e154c2a30ad7f3b4de1ed03bb15
Parker <parkerphillips2003@gmail.com>
2025-04-14 17:47:15 -0700

    Merging changes

--- src/main/java/com/parker/url/url_shortener/AuthenticationAPI/UserEndpoints.java
+++ src/main/java/com/parker/url/url_shortener/AuthenticationAPI/UserEndpoints.java
@@ -1,10 +1,8 @@
 package com.parker.url.url_shortener.AuthenticationAPI;
 
 import java.util.regex.Pattern;
-import java.time.Duration;
 import java.util.*;
 
-import org.apache.catalina.manager.util.SessionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import jakarta.servlet.http.Cookie;
 import org.springframework.http.HttpStatus;
@@ -16,8 +14,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
